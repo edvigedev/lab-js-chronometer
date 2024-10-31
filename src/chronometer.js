3294c19318c3c08b4d0693eabc5f084635026676
Edvige Di Salvo <edvigedis@google.com>
2024-10-31 17:48:42 +0100

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      console.log(this.currentTime); // This should now log the time every second
 
-  getMinutes() {
-    // ... your code goes here
-  }
-
-  getSeconds() {
-    // ... your code goes here
-  }
-
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+      // Check if printTimeCallback is provided and is a function before calling it
+      if (printTimeCallback && typeof printTimeCallback === "function") {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+  }
+
+  getMinutes() {
+    // Method implementation here
+  }
+
+  getSeconds() {
+    // Method implementation here
+  }
+
+  computeTwoDigitNumber(value) {
+    // Method implementation here
   }
 
   split() {
-    // ... your code goes here
+    // Method implementation here
   }
 }
